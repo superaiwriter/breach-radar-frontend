2536d51b1eedb2b12ca2513171697739a91f332d
AnuragSinghmin2 <221405anuragsingh@gmail.com>
2026-09-21 13:02:11 +0530

    fixing internal server error

--- src/services/api/client.js
+++ src/services/api/client.js
@@ -41,6 +41,7 @@ export const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
+  timeout: 15000,
 });
 
 let accessToken = getStoredAccessToken() || null;
